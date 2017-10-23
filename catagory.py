8b596e7ea35bfa40b2c837751bf7a544df3d3011
th4t-gi <juddbrau@gmail.com>
2017-10-22 21:37:58 -0600

    Open AP, UP, and criteria from packet. Next: make it "data"

--- catagory.py
+++ catagory.py
@@ -5,11 +5,11 @@ catagories = {
     ("Arts & Aesthetics", 1) : ['public', 'art', 'arts', 'aesthetics', 'design', 'picture', 'drawing'],
     ("Basic Needs", 2) : ['poverty', 'food', 'shelter', 'water', 'basic needs'],
     ("Business & Commerce", 3) : ['money', 'cash', 'stock', 'business', 'commerce'],
-    ("Communication", 4) : ['information', 'communication', 'talking', 'phones', 'connection'],
-    ("Defense", 5) : ['control', 'country', 'national', 'countries'],
+    ("Communication", 4) : ['information', 'communication', 'talking', 'phones', 'connection', 'discuss', 'tell'],
+    ("Defense", 5) : ['control', 'country', 'national', 'countries', 'protect', 'military'],
     ("Economics", 6) : ['economics', 'banking', 'bank', 'credit', 'credit card', 'money'],
-    ("Education", 7) : ['education', 'educating', 'educate', 'school', 'learn', 'teach'],
-    ("Environment", 8) : ['environment', 'plants', 'trees', 'animals', 'pollution'],
+    ("Education", 7) : ['education', 'educating', 'educate', 'school', 'learn', 'teach', 'math', 'science'],
+    ("Environment", 8) : ['environment', 'plants', 'trees', 'animals', 'pollution', ],
     ("Ethics & Religion", 9) : ['moral', 'right and wrong', 'standards', 'belief', 'religion', 'ethics', 'god'],
     ("Government & Politics", 10) : ['government', 'politics', 'countries', 'senate', 'law', 'house of representatives'],
     ("Law & Justice", 11) : ['law', 'justice', 'court', 'senate', 'judge'],
@@ -17,45 +17,76 @@ catagories = {
     ("Physical Health", 13) : ['physical health', 'hospital', 'hospitals', 'injury', 'injuries'],
     ("Psychological Health", 14) : ['counseling', 'therapy', 'phycological', 'health', 'crazy', 'mind', 'mental health'],
     ("Recreation", 15) : ['enjoyment', 'activity', 'pastime', 'relaxation', 'recreation'],
-    ("Social Relationships", 16) : ['social', 'relationship', 'individuals', 'agreement'],
+    ("Social Relationships", 16) : ['social', 'relationship', 'individuals', 'agreement', 'agreeing'],
     ("Technology", 17) : ['technology', 'computers', 'algorithms', 'innovations', 'electricity',
         'machinery', 'dna', 'future', '3d printing', 'ai', ' alien', 'space travel', 'space'],
     ("Transportation", 18) : ['transportation', 'cars', 'trains', 'airplanes', 'movement', 'vehicle']
 }
 
-def get_key_from_listval(d, val):
+class FPS_data(type):
+    """Documentation for FPS_data. Core of data used in packet analysis"""
 
-    cata = []
-    for l in d.values():
-        if val in l:
-            cata.append(catagories.keys()[catagories.values().index(l)][1])
 
+def get_key_from_list(val, dictionary):
+    d = multi_key_dict(dictionary)
+    cata = [d.keys()[d.values().index(l)]
+            for l in d.values() if val in l]
     return sorted(cata)
 
+
 def clean_up(group):
-    if type(group) is str:
-        group.translate(None, "\n")
-    group = re.split("\d+\.", group)
+    group = group.replace(',', '').replace('.', '')
+    group = re.split(r'\n\n', group)
     for string in group:
         if string.startswith("#"):
             group.remove(string)
 
     return group
 
-catagories = multi_key_dict(catagories)
-# use get_key_from_listval to find cata number
+def find_section(group, section):
+    go = re.search(r'(?P<name>%s(: \d)?)' % section, group)
+    # sect = []
+    sect = re.search(r'(\(%s\)(\n?.+)+)' % go.group("name"), group, re.M)
+    return sect.group(1)
 
-probs = open("Packet/ex_problems.txt")
-sols = open("Packet/ex_solutions.txt")
+def pos_cata(f):
+    analysis = []
+    rtrn = []
+    for problem in f:
+        words = []
+        for word in problem.split():
+            cata = get_key_from_list(word, catagories)
+            if cata:
+                words.append(cata)
+        analysis.append(words)
 
-problems = clean_up(probs.read())
-solutions = clean_up(sols.read())
+    i = 1
+    for each_prob in analysis:
+        if len(each_prob) == 1:
+            each_prob = [item for sublist in each_prob for item in sublist]
+        for idx, cata in enumerate(each_prob):
+            if len(cata) == 1:
+                each_prob[idx] = tuple([item for sublist in cata for item in sublist])
 
-for idx, p in enumerate(problems):
-    print str(idx + 1) + "." + p
-print "-------------------------------------------------------------------------------------------------------------"
-for idx, s in enumerate(solutions):
-    print str(idx + 1) + "." + s
+        each_prob.insert(0, str(i) + '.')
+        if len(each_prob) > 1:
+            rtrn.append(each_prob)
+        i += 1
+    return rtrn
 
-probs.close()
-sols.close()
+probs = clean_up(open("Packet/ex_problems.txt").read())
+sols = clean_up(open("Packet/ex_solutions.txt").read())
+misc = open("Packet/ex_UP+AP+S_Criteria+A_Criteria.txt").read()
+bla = FPS_data(12)
+# UP = find_section(misc, "UP")
+# AP = find_section(misc, "AP")
+# SC = find_section(misc, "Criteria")
+AC = find_section(misc, "Apply Criteria")
+print AC
+# for line in AP:
+#     print line
+# prob_cata = pos_cata(probs)
+# sol_cata = pos_cata(sols)
+# for sol in sol_cata:
+#     for item in sol:
+#         print item
