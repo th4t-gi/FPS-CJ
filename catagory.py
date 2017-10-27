843db0ac901c2686d437006025f8e43fe42dd724
th4t-gi <juddbrau@gmail.com>
2017-10-26 22:36:24 -0600

    finallize possible category function. Started on FPS_data() object

--- catagory.py
+++ catagory.py
@@ -11,7 +11,8 @@ catagories = {
     ("Education", 7) : ['education', 'educating', 'educate', 'school', 'learn', 'teach', 'math', 'science'],
     ("Environment", 8) : ['environment', 'plants', 'trees', 'animals', 'pollution', ],
     ("Ethics & Religion", 9) : ['moral', 'right and wrong', 'standards', 'belief', 'religion', 'ethics', 'god'],
-    ("Government & Politics", 10) : ['government', 'politics', 'countries', 'senate', 'law', 'house of representatives'],
+    ("Government & Politics", 10) : ['government', 'politics', 'countries', 'senate', 'law', 'house of representatives',
+        'prime minister'],
     ("Law & Justice", 11) : ['law', 'justice', 'court', 'senate', 'judge'],
     ("Misc", 12) : ['dna', 'future', '3d printing', 'ai', ' alien', 'space travel', 'space'],
     ("Physical Health", 13) : ['physical health', 'hospital', 'hospitals', 'injury', 'injuries'],
@@ -22,83 +23,86 @@ catagories = {
         'machinery', 'dna', 'future', '3d printing', 'ai', ' alien', 'space travel', 'space'],
     ("Transportation", 18) : ['transportation', 'cars', 'trains', 'airplanes', 'movement', 'vehicle']
 }
+multi_cata = multi_key_dict(catagories)
 
-# class FPS_data(type):
-#     """Documentation for FPS_data. 'Core' of data used in packet analysis"""
-#     def __init__(self, section, text, num=None):
-#         super(FPS_data, self).__init__()
-#         self.sect = section #AP, UP, problem, etc.
-#         self.num = num #This does not apply to UP, AP
-#         self.core = text
-#         self.misc = ["UP", "Criteria", "Apply Criteria", "AP"]
-#
-#         if self.sect in self.misc:
-#             self.core = self.find_section(self.core, self.sect)
-#
-#         if self.sect == "problem" or self.sect == "solution":
-#
-#
-#     def find_section(self, group, section):
-#         name = re.search(r'(?P<name>%s(: \d)?)' % section, group)
-#         sect = re.search(r'(\(%s\)(\n?.+)+)' % name.group("name"), group, re.M)
-#         return sect.group(1)
+class FPS_data(type):
+    """Documentation for FPS_data. 'Core' of data used in packet analysis"""
+    def __init__(self, section, text, num=None):
+        super(FPS_data, self).__init__()
+        self.sect = section #UP, problem, Criteria etc.
+        self.num = num #This does not apply to UP, AP
+        self.core = text
+        self.misc = ["up", "criteria", "apply criteria", "ap"]
 
-def pos_cata(f): #f stands for the file
-    pos = re.findall(r"(\d+\.) \(cata\: (?:(\d+)|(\d+)\,\s(\d+)|)\)", f)
-    analysis = [tuple([el for el in tup if el]) for tup in pos]
-    print analysis
+        if self.sect.lower() in self.misc:
+            self.core = self.find_section(self.core, self.sect)
 
-    # i = 1
-    # for each_prob in analysis:
-    #     if len(each_prob) == 1:
-    #         each_prob = [item for sublist in each_prob for item in sublist]
-    #     for idx, cata in enumerate(each_prob):
-    #         if len(cata) == 1:
-    #             each_prob[idx] = tuple([item for sublist in cata for item in sublist])
-    #
-    #     each_prob.insert(0, str(i) + '.')
-    #     if len(each_prob) > 1:
-    #         rtrn.append(each_prob)
-    #     i += 1
-    # return rtrn
+        if self.sect.lower() == "problem" or self.sect.lower() == "solution":
+            pass
 
+    def find_section(self, group, section):
+        name = re.search(r'(?P<name>%s(: \d)?)' % section, group)
+        sect = re.search(r'(\(%s\)(\n?.+)+)' % name.group("name"), group, re.M)
+        return sect.group(1)
 
-def get_key_from_list(val, dictionary):
-    try:
-        d = multi_key_dict(dictionary)
-        cata = [d.keys()[d.values().index(l)]
-                for l in d.values() if val in l]
+def pos_cata(f, mode, para=None): #f stands for the file; para is the paragraph to find keywords in
+    paragraph_nums = re.findall(r"(\d+)\. \(", f)
+    paragraph_nums = [int(num) for num in paragraph_nums]
+
+    if mode == "named":
+        pos = re.findall(r"\d+\. \(cata: (\d+|P|D)(?:\, (\d+)| -> \d+\.)?\)", f)
+        pos = [list(el) for el in zip(paragraph_nums, [list(tup) if tup[1] else tup[0] for tup in pos])]
+        dup = re.findall(r"(\d+)\. \(cata: (D) -> (\d+)\.\)", f)
+        dup = [[int(data[0]), dict((("D", int(data[2])),))] for data in dup]
+        for each in pos:
+            if type(each[1]) == list:
+                each[1] = get_cata_from(each[1])
+            elif each[1] == 'D':
+                each[0:2] = [elmt for elmt in dup if each[0] == elmt[0]][0]
+            elif not each[1] == 'P':
+                each[1] = get_cata_from(int(each[1]))
+
+        return pos
+
+    if mode == "num":
+        paragraphs = re.split(r"\d+\. \(.*?\) ", f)
+        paragraphs = [item.replace("\n", '') for item in paragraphs
+                    if not item.startswith("#")]
+        analysis = []
+        for idx, p in enumerate(paragraphs):
+            p = p.replace(".", '').replace(",", '').split()
+            keywords = [get_cata_from(word) for word in p if get_cata_from(word)]
+            if keywords and not para:
+                if len(keywords) == 1: keywords = keywords[0]
+                analysis.append([paragraph_nums[idx], keywords])
+
+            elif para == idx + 1:
+                keywords = [get_cata_from(word) for word in p if get_cata_from(word)]
+                if not keywords: return "No keywords found"
+                return keywords
+        return analysis
+
+def get_cata_from(val):
+    if type(val) == str:
+        cata = [multi_cata.keys()[multi_cata.values().index(l)]
+                for l in multi_cata.values() if val in l]
+    elif type(val) == int:
+        cata = [multi_cata.keys()[idx] for idx, key in
+                enumerate(multi_cata.keys()) if key[1] == val]
+    elif type(val) == list:
+        cata = []
+        for elmt in val:
+            cata.append([multi_cata.keys()[idx] for idx, key in
+                    enumerate(multi_cata.keys()) if key[1] == int(elmt)][0])
+    if len(cata) >= 2:
         return sorted(cata)
-    except Exception:
-        print """KeyError: Keys of dictionary must be of type tup not {0}.
-        """.format(type(dictionary).__name__)
-        exit()
-
-# def clean_up(group):
-#     group = group.replace(',', '').replace('.', '')
-#     group = re.split(r'\n\n', group)
-#     for string in group:
-#         if string.startswith("#"):
-#             group.remove(string)
-#
-#     return group
+    elif len(cata) == 1:
+        cata = cata[0]
+        return cata
 
 probs = open("Packet/ex_problems.txt").read()
-# print probs
-# probs = clean_up(probs)
-# print "\n", probs
-# sols = clean_up(open("Packet/ex_solutions.txt").read())
+sols = open("Packet/ex_solutions.txt").read()
 misc = open("Packet/ex_UP+AP+S_Criteria+A_Criteria.txt").read()
-# UP = find_section(misc, "UP")
-# AP = find_section(misc, "AP")
-# SC = find_section(misc, "Criteria")
-# AC = find_section(misc, "Apply Criteria")
-# print AC
-# for line in AP:
-#     print line
-prob_cata = pos_cata(probs)
-# print "\n", prob_cata
-# sol_cata = pos_cata(sols)
-# for sol in sol_cata:
-#     for item in sol:
-#         print item
+
+prob_cata = pos_cata(probs, "named")
+sol_cata = pos_cata(sols, "named")
