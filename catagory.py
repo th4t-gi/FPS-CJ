6a36ad2c7cf32941fbf440f8c81874d0d1a6428b
th4t-gi <juddbrau@gmail.com>
2017-10-27 12:37:51 -0600

    Added pos_cata() to FPS_data object

--- catagory.py
+++ catagory.py
@@ -5,18 +5,20 @@ catagories = {
     ("Arts & Aesthetics", 1) : ['public', 'art', 'arts', 'aesthetics', 'design', 'picture', 'drawing'],
     ("Basic Needs", 2) : ['poverty', 'food', 'shelter', 'water', 'basic needs'],
     ("Business & Commerce", 3) : ['money', 'cash', 'stock', 'business', 'commerce'],
-    ("Communication", 4) : ['information', 'communication', 'talking', 'phones', 'connection', 'discuss', 'tell'],
+    ("Communication", 4) : ['information', 'communication', 'talking', 'phones', 'connection', 'discuss',
+        'tell'],
     ("Defense", 5) : ['control', 'country', 'national', 'countries', 'protect', 'military'],
     ("Economics", 6) : ['economics', 'banking', 'bank', 'credit', 'credit card', 'money'],
     ("Education", 7) : ['education', 'educating', 'educate', 'school', 'learn', 'teach', 'math', 'science'],
     ("Environment", 8) : ['environment', 'plants', 'trees', 'animals', 'pollution', ],
     ("Ethics & Religion", 9) : ['moral', 'right and wrong', 'standards', 'belief', 'religion', 'ethics', 'god'],
-    ("Government & Politics", 10) : ['government', 'politics', 'countries', 'senate', 'law', 'house of representatives',
-        'prime minister'],
+    ("Government & Politics", 10) : ['government', 'politics', 'countries', 'senate', 'law',
+        'house of representatives', 'prime minister'],
     ("Law & Justice", 11) : ['law', 'justice', 'court', 'senate', 'judge'],
     ("Misc", 12) : ['dna', 'future', '3d printing', 'ai', ' alien', 'space travel', 'space'],
     ("Physical Health", 13) : ['physical health', 'hospital', 'hospitals', 'injury', 'injuries'],
-    ("Psychological Health", 14) : ['counseling', 'therapy', 'phycological', 'health', 'crazy', 'mind', 'mental health'],
+    ("Psychological Health", 14) : ['counseling', 'therapy', 'phycological', 'health', 'crazy', 'mind',
+        'mental health'],
     ("Recreation", 15) : ['enjoyment', 'activity', 'pastime', 'relaxation', 'recreation'],
     ("Social Relationships", 16) : ['social', 'relationship', 'individuals', 'agreement', 'agreeing'],
     ("Technology", 17) : ['technology', 'computers', 'algorithms', 'innovations', 'electricity',
@@ -25,84 +27,90 @@ catagories = {
 }
 multi_cata = multi_key_dict(catagories)
 
-class FPS_data(type):
-    """Documentation for FPS_data. 'Core' of data used in packet analysis"""
+class FPS_data(object):
+    """Documentation for FPS_data. 'Core' data object used in packet analysis"""
+
     def __init__(self, section, text, num=None):
         super(FPS_data, self).__init__()
-        self.sect = section #UP, problem, Criteria etc.
-        self.num = num #This does not apply to UP, AP
+        self.sect = section.lower() #UP, problem, Criteria etc.
         self.core = text
+        self.num = num #This does not apply to UP, AP
         self.misc = ["up", "criteria", "apply criteria", "ap"]
+        self.data = None
 
-        if self.sect.lower() in self.misc:
+        if self.sect in self.misc:
             self.core = self.find_section(self.core, self.sect)
+            # self.data =
 
-        if self.sect.lower() == "problem" or self.sect.lower() == "solution":
-            pass
+
+        if self.sect == "problem" or self.sect == "solution":
+            self.data = self.pos_cata("num", num)
 
     def find_section(self, group, section):
-        name = re.search(r'(?P<name>%s(: \d)?)' % section, group)
-        sect = re.search(r'(\(%s\)(\n?.+)+)' % name.group("name"), group, re.M)
+        name = re.search(r'(%s(?:\: (\d))?)' % section, group, flags=re.I)
+        sect = re.search(r'(\(%s\) (\n?.+)+)' % name.group(1), group, flags=re.M)
         return sect.group(1)
 
-def pos_cata(f, mode, para=None): #f stands for the file; para is the paragraph to find keywords in
-    paragraph_nums = re.findall(r"(\d+)\. \(", f)
-    paragraph_nums = [int(num) for num in paragraph_nums]
+    def pos_cata(self, mode, para): #f stands for the file; para is the paragraph to find keywords in
+        paragraph_nums = re.findall(r"(\d+)\. \(", self.core)
+        paragraph_nums = [int(num) for num in paragraph_nums]
 
-    if mode == "named":
-        pos = re.findall(r"\d+\. \(cata: (\d+|P|D)(?:\, (\d+)| -> \d+\.)?\)", f)
-        pos = [list(el) for el in zip(paragraph_nums, [list(tup) if tup[1] else tup[0] for tup in pos])]
-        dup = re.findall(r"(\d+)\. \(cata: (D) -> (\d+)\.\)", f)
-        dup = [[int(data[0]), dict((("D", int(data[2])),))] for data in dup]
-        for each in pos:
-            if type(each[1]) == list:
-                each[1] = get_cata_from(each[1])
-            elif each[1] == 'D':
-                each[0:2] = [elmt for elmt in dup if each[0] == elmt[0]][0]
-            elif not each[1] == 'P':
-                each[1] = get_cata_from(int(each[1]))
+        if mode == "num": #mode for finding judges decision
+            dup = re.search(r"^{0}\. \(cata: (D) -> (\d+)\.\)".format(para), self.core, re.M)
+            if dup: return [para, {dup.group(1): int(dup.group(2))}]
 
-        return pos
+            pos = re.search(r"{0}\. \(cata: (\d+|P)(?:\, (\d+))?\)".format(para), self.core)
+            if pos.group(2): return [para, get_cata_from([pos.group(1), pos.group(2)])]
+            return [para, get_cata_from(pos.group(1))]
 
-    if mode == "num":
-        paragraphs = re.split(r"\d+\. \(.*?\) ", f)
-        paragraphs = [item.replace("\n", '') for item in paragraphs
-                    if not item.startswith("#")]
-        analysis = []
-        for idx, p in enumerate(paragraphs):
-            p = p.replace(".", '').replace(",", '').split()
-            keywords = [get_cata_from(word) for word in p if get_cata_from(word)]
-            if keywords and not para:
-                if len(keywords) == 1: keywords = keywords[0]
-                analysis.append([paragraph_nums[idx], keywords])
-
-            elif para == idx + 1:
+        if mode == "named": #mode for finding keywords from each problem
+            paragraphs = re.split(r"\d+\. \(.*?\) ", self.core)
+            paragraphs = [item.replace("\n", '') for item in paragraphs
+                        if not item.startswith("#")]
+            analysis = []
+            for idx, p in enumerate(paragraphs):
+                p = p.replace(".", '').replace(",", '').split()
                 keywords = [get_cata_from(word) for word in p if get_cata_from(word)]
-                if not keywords: return "No keywords found"
-                return keywords
-        return analysis
+                if keywords and not para:
+                    if len(keywords) == 1: keywords = keywords[0]
+                    analysis.append([paragraph_nums[idx], keywords])
+
+                elif para == idx + 1:
+                    keywords = [get_cata_from(word) for word in p if get_cata_from(word)]
+                    if not keywords: return "No keywords found"
+                    return keywords
+            return analysis
 
 def get_cata_from(val):
+    if val == "P":
+        return val
+
     if type(val) == str:
-        cata = [multi_cata.keys()[multi_cata.values().index(l)]
-                for l in multi_cata.values() if val in l]
-    elif type(val) == int:
-        cata = [multi_cata.keys()[idx] for idx, key in
-                enumerate(multi_cata.keys()) if key[1] == val]
+        if re.match(r"\d+", val):
+            cata = [multi_cata.keys()[idx] for idx, key in
+                enumerate(multi_cata.keys()) if key[1] == int(val)]
+        else:
+            cata = [multi_cata.keys()[multi_cata.values().index(l)]
+                    for l in multi_cata.values() if val in l]
+
     elif type(val) == list:
         cata = []
         for elmt in val:
             cata.append([multi_cata.keys()[idx] for idx, key in
                     enumerate(multi_cata.keys()) if key[1] == int(elmt)][0])
+
     if len(cata) >= 2:
         return sorted(cata)
     elif len(cata) == 1:
-        cata = cata[0]
-        return cata
+        return cata[0]
 
 probs = open("Packet/ex_problems.txt").read()
 sols = open("Packet/ex_solutions.txt").read()
 misc = open("Packet/ex_UP+AP+S_Criteria+A_Criteria.txt").read()
 
-prob_cata = pos_cata(probs, "named")
-sol_cata = pos_cata(sols, "named")
+UP = FPS_data("UP", misc)
+prob_cata = []
+sol_cata = []
+for i in range(1, 17):
+    prob_cata.append(FPS_data("problem", probs, i))
+    sol_cata.append(FPS_data("solution", sols, i))
