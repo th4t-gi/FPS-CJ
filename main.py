f9374489319ada23bfc802d2dd99bea15421db23
th4t-gi <juddbrau@gmail.com>
2017-11-21 09:20:28 -0700

    Half way towards a packet-grading ANN

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from catagory import *
+from os import listdir
+from catagory import FPS
 from nn.CANN import CataNN
 
+packet = "q j102 packet/"#raw_input("path to packet files: ") + '/'
+def find(keyword, array):
+    return [i for i in array if keyword in i][0]
+try:
+    probs = open(packet + find('prob', listdir(packet))).read()
+    sols = open(packet + find('sol', listdir(packet))).read()
+    misc = open(packet + find('misc', listdir(packet))).read()
+except OSError:
+    print "invalid packet directory"
+    quit()
+
 prob_cata = [FPS("problem", probs, i) for i in range(1, 17)]
+# sol_cata = [FPS("solution", sols, i) for i in range(1, 17)]
 UP = FPS("UP", misc)
-sol_cata = [FPS("solution", sols, i) for i in range(1, 17)]
-SC = FPS("Criteria", misc)
-AC = FPS("Apply Criteria", misc)
-AP = FPS("AP", misc)
+# SC = FPS("Criteria", misc)
+# AC = FPS("Apply Criteria", misc)
+# AP = FPS("AP", misc)
+
+p0 = CataNN([18], prob_cata[0].core, prob_cata[0].data)
 
 total_points = 0
 
--- nn/CANN.py
+++ nn/CANN.py
@@ -1,5 +1,46 @@
+import numpy as np
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import stopwords
+import pickle
+np.random.seed(1)
 
-class CataNN():
 
-    def __init__(self, sect):
+class _neuralnet(object):
+
+    def __init__(self, input, layers):
+        if type(layers) != list:
+            self._type = type(layers).__name__
+            raise TypeError("layers parameter needs to be type list not type {0}".format(self._type))
+
+        self.out = layers[-1]
+        self.hidden = layers[:-1]
+
+    def nonlinear(self, x, deriv=False):
+        if deriv:
+            return x*(1 - x)
+        return 1 / (1 + np.exp(-x))
+
+    def cost(self, guessed):
+        return (guessed - self.result)*(guessed - self.result)
+
+    def train(self, iter):
         pass
+
+class CataNN(_neuralnet):
+
+    def __init__(self, struct, core, data):
+        """A child neural network class for classifying the problems and solutions
+        in a FPS packet"""
+
+        if not((isinstance(struct, list) and len(struct) == 2) or isinstance(core, str) or isinstance(data, list)):
+            raise NameError("CataNN.__init__ arguments are of incorrect format")
+
+        core = word_tokenize(core)
+        self.o = [word for word in [core[(i + 1):] for i, el in enumerate(core) if el == ')'][0]
+                if word.lower() not in stopwords.words('english') and len(word) > 1]
+        self.o = self.w2v(self.o)
+
+        super(CataNN, self).__init__(self.o, struct)
+
+    def w2v(self, words):
+        return words
