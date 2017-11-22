dcc09f15b3a74c92292a97bf25efece03cd78f0b
th4t-gi <juddbrau@gmail.com>
2017-11-21 22:09:40 -0700

    just formating fixes.  Still need to do word2vec for inputs

--- nn/CANN.py
+++ nn/CANN.py
@@ -1,8 +1,6 @@
-import numpy as np
+import numpy as np, pickle, re
 from nltk import word_tokenize, pos_tag
 from nltk.corpus import stopwords
-import pickle
-np.random.seed(1)
 
 
 class _neuralnet(object):
@@ -35,12 +33,31 @@ class CataNN(_neuralnet):
         if not((isinstance(struct, list) and len(struct) == 2) or isinstance(core, str) or isinstance(data, list)):
             raise NameError("CataNN.__init__ arguments are of incorrect format")
 
-        core = word_tokenize(core)
+        self.contrxs = {"aren't" : "are not", "can't" : "cannot", "couldn't" : "could not",
+                "didn't" : "did not", "doesn't" : "does not", "don't" : "do not",
+                "hadn't" : "had not", "hasn't" : "has not", "haven't" : "have not",
+                "he'd" : "he had", "he'll" : "he will", "he's" : "he is; he has",
+                "I'd" : "I had", "I'll" : "I will", "I'm" : "I am", "I've" : "I have",
+                "isn't" : "is not", "let's" : "let us", "mightn't" : "might not",
+                "mustn't" : "must not", "shan't" : "shall not", "she'd" : "she had",
+                "she'll" : "she will", "she's" : "she is", "shouldn't" : "should not",
+                "that's" : "that is", "there's" : "there is", "they'd" : "they had",
+                "they'll" : "they will", "they're" : "they are", "they've" : "they have",
+                "we'd" : "we had", "we're" : "we are", "we've" : "we have",
+                "weren't" : "were not", "what'll" : "what will", "what're" : "what are",
+                "what's" : "what is", "what've" : "what have", "where's" : "where is",
+                "who's" : "who had", "who'll" : "who will", "who're" : "who are",
+                "who's" : "who is", "who've" : "who have", "won't" : "will not",
+                "wouldn't" : "would not", "you'd" : "you had", "you'll" : "you will",
+                "you're" : "you are", "you've" : "you have"}
+
+        core = ' '.join([self.contrxs[word] if word in self.contrxs.keys() else word for word in core.split()])
+        core = word_tokenize(re.sub(r" \(not sure\)", '', core))
         self.o = [word for word in [core[(i + 1):] for i, el in enumerate(core) if el == ')'][0]
-                if word.lower() not in stopwords.words('english') and len(word) > 1]
-        self.o = self.w2v(self.o)
+                if word.lower() not in stopwords.words('english') and not re.search(r"(\,|\'|\.|\(|\)|\-)", word)]
+        self.w2v()
 
         super(CataNN, self).__init__(self.o, struct)
 
-    def w2v(self, words):
-        return words
+    def w2v(self):
+        print self.o
