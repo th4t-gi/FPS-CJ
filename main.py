7d6fd05a19755c928723a2ca8e20a39168e0c770
th4t-gi <juddbrau@gmail.com>
2018-09-08 22:04:45 -0600

    changed so multiple judges can judge same packet

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
-import os, re, subprocess
-from catagory import FPS
-from nn.CANN import CataNN
+import os, re
+from catagory import FPS, find_packet
+# from nn.CANN import CataNN
 
+#lowercases every element in a given list
+def integrity(list):
+    _list = []
+    for i in list:
+        _list.append(i.lower())
+    return _list
 
-packet = raw_input("folder of packet: ").title()
+#finds the directory path of given folder
+
+packet = raw_input("folder name of packet: ").upper()
+if not packet:
+    packet = "CO-15-16-J102-S"
+flashdrive = raw_input("Is the file on a USB drive?(yes/no): ")
+
+if flashdrive == "yes":
+    flashdrive = raw_input("What is the name of the USB drive?: ")
+    if flashdrive.lower() in integrity(os.listdir("/Volumes")):
+        os.chdir("/Volumes/{}".format(flashdrive))
+        packet_path = find_packet(packet, os.getcwd())
+else:
+    print "OK"
+    packet_path = find_packet(packet, "~/")
+    packet_path = ''.join(re.split(re.compile(r"({})".format(packet)), packet_path)[:2]) + "/"
 
 try:
-    packet_path = subprocess.check_output("find ~/Code/Project\ folder/ -type d -name \"{}\" -print".format(packet),shell=True,stderr=subprocess.STDOUT)
-except subprocess.CalledProcessError as e:
-    packet_path = e.output
-
-if not packet_path.endswith("/"):
-    packet_path = packet_path.replace('\n', '') + "/"
-def find(array, keyword):
-    return [i for i in array if keyword in i][0]
-
-try:
-    probs = open(packet_path + find(os.listdir(packet_path), 'prob')).read()
-    sols = open(packet_path + find(os.listdir(packet_path), 'sol')).read()
-    misc = open(packet_path + find(os.listdir(packet_path), 'up')).read()
+    probs = [open(packet_path + i).read() for i in os.listdir(packet_path) if 'prob' in i]
+    sols = [open(packet_path + i).read() for i in os.listdir(packet_path) if 'sol' in i]
+    misc = [open(packet_path + i).read() for i in os.listdir(packet_path) if 'up' in i]
 except OSError:
-    print "invalid packet directory"
+    print "invalid packet directory: {}".format(packet)
     quit()
 except IndexError:
-    print "packet file names need to be Uncapitalized"
+    print "packet file(s) not found"
 
-prob_cata = [FPS("problem", probs, i) for i in range(1, 17)]
+# prob1 = FPS("problem", probs, 1)
+# prob_cata = [FPS("problem", probs, i) for i in range(1, 17)]
 # sol_cata = [FPS("solution", sols, i) for i in range(1, 17)]
-UP = FPS("UP", misc)
+# UP = FPS("UP", misc)
 # SC = FPS("Criteria", misc)
-# AC = FPS("Apply Criteria", misc)
+# AC = FPS("grid", misc)
 # AP = FPS("AP", misc)
 
 num = 4
-p0 = CataNN([18], prob_cata[num].core, prob_cata[num].data)
+# p0 = CataNN([18], prob_cata[num].core, prob_cata[num].data)
 # pnn = [CataNN([18], i.core, i.data) for i in prob_cata]
 
 
 total_points = 0
-
-#------------------------------problems------------------------------
-
-
-#---------------------------------UP---------------------------------
-
-
-#-----------------------------solutions------------------------------
-
-
-#------------------------------criteria------------------------------
-
-
-#---------------------------apply criteria---------------------------
-
-
-#---------------------------------AP---------------------------------
