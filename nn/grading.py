832d123c95326e494f69d8949445ac13ae7e3a23
th4t-gi <juddbrau@gmail.com>
2018-09-25 21:53:57 -0600

    This is where the neural networks are initiated and "report" to.

---
+++ nn/grading.py
@@ -0,0 +1,9 @@
+from core import neuralnet
+
+framework = {"CANN": neuralnet,
+             "YR": neuralnet,
+             "EN": neuralnet,
+             "UP": neuralnet,
+             "AP": neuralnet,
+             "R4": neuralnet,
+             }
