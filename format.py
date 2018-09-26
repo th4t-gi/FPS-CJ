fb07d60541e0cd63a6f3ccd268fcbe5a8abf6fd4
th4t-gi <juddbrau@gmail.com>
2018-09-25 21:55:15 -0600

    Made it so more of the data formating is handled by format.py

--- format.py
+++ format.py
@@ -3,7 +3,7 @@ t = time.time()
 from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
-import subprocess, re
+import subprocess, re, os
 
 #finds the directory path of given folder
 def find_packet(file, rootdir="~/", dir=True):
@@ -19,6 +19,38 @@ def find_packet(file, rootdir="~/", dir=True):
         _packet_path = _packet_path.replace('\n', '')
     return _packet_path
 
+def get_packet():
+    packet = raw_input("folder name of packet: ").upper()
+    flashdrive = "no" #raw_input("Is the file on a USB drive?(yes/no): ")
+
+    if flashdrive == "yes":
+        flashdrive = raw_input("What is the name of the USB drive?: ")
+        if flashdrive.lower() in [item.lower() for item in os.listdir("/Volumes")]:
+            os.chdir("/Volumes/{}".format(flashdrive))
+            paths = find_packet(packet, os.getcwd())
+    elif packet:
+        print "OK"
+        paths = find_packet(packet, "~/Code/")
+        paths = ''.join(re.split(re.compile(r"({})".format(packet)), paths)[:2]) + "/"
+    #finds path to given packet, or all packets
+    try:
+        if not packet:
+            print "OK"
+            paths = subprocess.check_output("find ~/Code/ -regex \".*/data-[^score].*\.json\" -type f",shell=True,stderr=subprocess.STDOUT)
+            paths = [i for i in paths.split("\n") if i]
+        else:
+            paths = [paths + i for i in os.listdir(paths) if 'data' in i]
+    except OSError:
+        print "invalid packet directory: {}".format(packet)
+        quit()
+    except IndexError:
+        print "packet file(s) not found"
+    except subprocess.CalledProcessError as e:
+        paths = e.output
+
+    return [packet, paths]
+
+
 def text(key, dictionary, path=False):
     for k, v in dictionary.iteritems():
         if re.match(key, k): yield v
@@ -30,22 +62,19 @@ def text(key, dictionary, path=False):
                 if type(d) == dict:
                     for result in text(key, d): yield result
 
-def word2vec(l):
-    if type(l[0]) == list:
-        l = l[0]
+def word2vec(l, packet=True):
     # split each string into words
-    l = [string.split(" ") for string in [re.sub(r"[^\w^ ]", "", item).lower() for item in l]]
-
+    if not packet:
+        l = [re.sub(r"[^\w^ ]", "", re.sub(r"/", " ", string)).lower().split(" ") for packet in l for string in packet]
+    else:
+        l = [re.sub(r"[^\w^ ]", "", string).lower().split(" ") for string in l[0]]
     # train model
     model = Word2Vec(l, min_count=1)
     # fit a 2d PCA model to the vectors
     X = model[model.wv.vocab]
     pca = PCA(n_components=2)
     result = pca.fit_transform(X)
-    # create a scatter plot of the projection
-    pyplot.scatter(result[:, 0], result[:, 1])
     words = [word for word in list(model.wv.vocab) if word]
-
     formatted = dict(zip(words, X))
     return formatted
 
--- main.py
+++ main.py
@@ -3,60 +3,26 @@ t = time.time()
 import os, re, json, subprocess, warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import format
-from nn.CANN import CataNN
-#lowercases every element in a given list
-def integrity(list):
-    _list = []
-    for i in list:
-        _list.append(i.lower())
-    return _list
+from nn.grading import framework
+
+p = format.get_packet()
+packet = p[0]
+paths = p[1]
+
+for i, val in enumerate(paths):
+    fuzzy = format.find_packet("fuzzy.txt", re.sub(r"data-\d{3}\.json", "", paths[i]).replace(" ", "\\ "), dir=False)
+    jeff = open(paths[i]).read()
+    with open(val, "r+") as f:
+        f.write(re.sub(r"future-scene\": \".*?\"", "future-scene\": \"{}\"".format(fuzzy),
+                re.sub(r"data-filepath\": \".*?\"", "data-filepath\": \"{}\"".format(val), jeff)))
+
+dadata = [json.loads(open(i).read()) for i in paths]
+d = [list(format.text(r".*?text", data)) for data in dadata]
+vecs = format.word2vec(d, packet)
 
 
-packet = "CO-18-M205-S"#raw_input("folder name of packet: ").upper()
-flashdrive = "no" #raw_input("Is the file on a USB drive?(yes/no): ")
+#    Neural
+#    Network
+#     Stuff
 
-if flashdrive == "yes":
-    flashdrive = raw_input("What is the name of the USB drive?: ")
-    if flashdrive.lower() in integrity(os.listdir("/Volumes")):
-        os.chdir("/Volumes/{}".format(flashdrive))
-        packet_path = format.find_packet(packet, os.getcwd())
-elif packet:
-    print "OK"
-    packet_path = format.find_packet(packet, "~/Code/")
-    packet_path = ''.join(re.split(re.compile(r"({})".format(packet)), packet_path)[:2]) + "/"
-
-try:
-    if not packet:
-        paths = subprocess.check_output("find ~/Code/ -regex \".*/data-[0-9][0-9][0-9]\.json\" -type f",shell=True,stderr=subprocess.STDOUT)
-        paths = paths.split("\n")
-
-    else:
-        paths = [packet_path + i for i in os.listdir(packet_path) if 'data' in i]
-except OSError:
-    print "invalid packet directory: {}".format(packet)
-    quit()
-except IndexError: print "packet file(s) not found"
-except subprocess.CalledProcessError as e:
-    paths = e.output
-
-dadata = []
-for i in [i for i in paths if i]:
-    with open(i) as file:
-        f = json.loads(file.read())
-        dadata.append(f)
-
-d = []
-comments = []
-for data in dadata:
-    d.append(list(format.text(r".*?text", data)))
-    comments.append(list(format.text(r"comments|feedback", data)))
-
-vecs = format.word2vec(d)
-
-num = 4
-# p0 = CataNN([18], prob_cata[num].core, prob_cata[num].data)
-# pnn = [CataNN([18], i.core, i.data) for i in prob_cata]
-
-
-total_points = 0
-print time.time()- t
+print "time:", round(time.time()- t, 3)
