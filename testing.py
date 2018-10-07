845a32a213f229d6ad9a36433dc2843b2addc93a
th4t-gi <juddbrau@gmail.com>
2018-10-06 20:57:12 -0600

    Trying to use tesorflow with El capitan... not working out that well.

--- testing.py
+++ testing.py
@@ -1,24 +1,10 @@
-import warnings
-from gensim.models import Word2Vec
-from sklearn.decomposition import PCA
-from matplotlib import pyplot
+import time
+t = time.time()
+import tensorflow as tf
+import keras
+from keras.layers import LSTM, Bidirectional, Dense, Input
+from keras.utils import plot_model
+from keras.models import Model
+import matplotlib
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
-# define training data
-sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-			['this', 'is', 'the', 'second', 'sentence'],
-			['yet', 'another', 'sentence'],
-			['one', 'more', 'sentence'],
-			['and', 'the', 'final', 'sentence']]
-# train model
-model = Word2Vec(sentences, min_count=1)
-# fit a 2d PCA model to the vectors
-X = model[model.wv.vocab]
-pca = PCA(n_components=2)
-result = pca.fit_transform(X)
-# create a scatter plot of the projection
-pyplot.scatter(result[:, 0], result[:, 1])
-words = list(model.wv.vocab)
-for i, word in enumerate(words):
-	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
-pyplot.show()
+model = LSTM(25, return_sequences=True)
