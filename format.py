85befabc957287e48773979c40e42f085091cff0
th4t-gi <juddbrau@gmail.com>
2018-10-15 14:24:27 -0600

    I realized that the data needed to be tokened for each Neural Network.

--- format.py
+++ format.py
@@ -1,16 +1,16 @@
 import time
 t = time.time()
+from nltk.tokenize import word_tokenize
 from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
-import subprocess, re, os
+from collections import namedtuple
+import subprocess, re, os, numpy as np, random, string, warnings
 
 #finds the directory path of given folder
 def find_packet(file, rootdir="~/", dir=True):
-    if dir:
-        d = "d"
-    else:
-        d = "f"
+    if dir: d = "d"
+    else: d = "f"
     try:
         _packet_path = subprocess.check_output("find {} -type {} -name \"{}\"".format(rootdir, d, file),shell=True,stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
@@ -19,63 +19,139 @@ def find_packet(file, rootdir="~/", dir=True):
         _packet_path = _packet_path.replace('\n', '')
     return _packet_path
 
-def get_packet():
-    packet = raw_input("folder name of packet: ").upper()
-    flashdrive = "no" #raw_input("Is the file on a USB drive?(yes/no): ")
+class packet(object):
 
-    if flashdrive == "yes":
-        flashdrive = raw_input("What is the name of the USB drive?: ")
-        if flashdrive.lower() in [item.lower() for item in os.listdir("/Volumes")]:
-            os.chdir("/Volumes/{}".format(flashdrive))
-            paths = find_packet(packet, os.getcwd())
-    elif packet:
-        print "OK"
-        paths = find_packet(packet, "~/Code/")
-        paths = ''.join(re.split(re.compile(r"({})".format(packet)), paths)[:2]) + "/"
-    #finds path to given packet, or all packets
-    try:
-        if not packet:
-            print "OK"
+    def __init__(self, *args):
+        super(packet, self).__init__()
+        self.packets = flatten(args)
+        self.packets = list(set(flatten([self.get_packets(i) for i in self.packets])))
+        self.paths = [self.get_data(i) for i in self.packets]
+        self.paths = [j for i in self.paths for j in i if not j == "NO PACKET"]
+
+    def get_data(self, packet):
+        self.packet = packet
+        if not self.packet:
+            return ["NO PACKET"]
+        self.requested_packet = not(self.packet.lower() == "all")
+        self.packet = self.packet.upper()
+        self.flashdrive = "no" #raw_input("Is the packet {} on a USB drive?(yes/no): ".format(self.packet))
+
+        if self.flashdrive == "yes":
+            self.flashdrive = raw_input("What is the name of the USB drive?: ")
+            if self.flashdrive.lower() in [item.lower() for item in os.listdir("/Volumes")]:
+                os.chdir("/Volumes/{}".format(self.flashdrive))
+                paths = find_packet(packet, os.getcwd())
+        #finds path to given packet, or all packets
+        try:
+            self.data = find_packet(self.packet, "~/Code/")
+            isda = self.isdata(self.packet)
+            if not isda:
+                return ["NO PACKET"]
+            self.data = ''.join(re.split(re.compile(r"({})".format(self.packet)), self.data)[:2]) + "/"
+            self.data = [self.data + i for i in os.listdir(self.data) if 'data' in i]
+            print "\033[1mFound Packet:\033[0m", self.packet
+        except subprocess.CalledProcessError as e:
+            self.data = e.output
+        except OSError:
+            print "invalid packet directory: {}".format(self.packet)
+            quit()
+        try:
+            return self.data
+        except IndexError:
+            print "packet file(s) not found"
+
+    def get_packets(self, packet):
+        if packet == "ALL":
             paths = subprocess.check_output("find ~/Code/ -regex \".*/data-[^score].*\.json\" -type f",shell=True,stderr=subprocess.STDOUT)
-            paths = [i for i in paths.split("\n") if i]
-        else:
-            paths = [paths + i for i in os.listdir(paths) if 'data' in i]
-    except OSError:
-        print "invalid packet directory: {}".format(packet)
-        quit()
-    except IndexError:
-        print "packet file(s) not found"
-    except subprocess.CalledProcessError as e:
-        paths = e.output
+            paths = [i for i in paths.split("\n") if i and os.path.basename(os.path.dirname(i)) not in self.packets]
+            packet = [os.path.basename(os.path.dirname(i)) for i in paths]
+        return packet
 
-    return [packet, paths]
+    def isdata(self, p):
+        if not self.data:
+            print "\033[91m{}\033[0m is not an FPS packet".format(str(p))
+            return False
+        else: return True
 
 
-def text(key, dictionary, path=False):
+class categorizedData(object):
+
+    def __init__(self, data):
+        super(categorizedData, self).__init__()
+        self.gp = ["categories", "perhaps", "why", "duplicate", "solution"]
+        #finds text from data obj
+        text = [list(getData(r".*?text", packet)) for packet in data]
+        self.tokens = token(text)
+        #finds categories for self.tokens
+        self.cats = [[c["scoring"].items() for c in packet["data"]] for packet in data]
+        self.cats = [[[tup for tup in c if tup[0] in self.gp] for c in packet] for packet in self.cats]
+        self.cats = [[i for c in packet for i in c if i[1]] for packet in self.cats]
+        #combines self.tokens and self.cats
+        finaldata = [zip(self.cats[i], packet) for i, packet in enumerate(self.tokens)]
+        #if multiple packets are inputed, it combines packets for training
+        self.data = flatten(finaldata, 2)
+
+
+def getData(key, dictionary, track=False):
     for k, v in dictionary.iteritems():
         if re.match(key, k): yield v
         elif isinstance(v, dict):
-            for result in text(key, v):
+            for result in getData(key, v):
                 yield result
         elif isinstance(v, list):
-            for d in v:
+            for i, d in enumerate(v):
+                if track:
+                    yield i
                 if type(d) == dict:
-                    for result in text(key, d): yield result
+                    for result in getData(key, d): yield result
 
-def word2vec(l, packet=True):
-    # split each string into words
-    if not packet:
-        l = [re.sub(r"[^\w^ ]", "", re.sub(r"/", " ", string)).lower().split(" ") for packet in l for string in packet]
-    else:
-        l = [re.sub(r"[^\w^ ]", "", string).lower().split(" ") for string in l[0]]
+def vectorize(l, show=False, size=100, sim=[]):
+    warnings.simplefilter(action="ignore", category=FutureWarning)
     # train model
-    model = Word2Vec(l, min_count=1)
-    # fit a 2d PCA model to the vectors
+    model = Word2Vec(l, min_count=1, size=size)
     X = model[model.wv.vocab]
-    pca = PCA(n_components=2)
-    result = pca.fit_transform(X)
     words = [word for word in list(model.wv.vocab) if word]
     formatted = dict(zip(words, X))
+    if sim and len(sim) in [2, 3]:
+        if len(sim) == 2:
+            result = model.most_similar(positive=[sim[0]], negative=[sim[1]], topn=5)
+        else:
+            result = model.most_similar(positive=[sim[0], sim[2]], negative=[sim[1]], topn=5)
+        print result
+    if show:
+        # create a scatter plot of the projection
+        pca = PCA(n_components=2)
+        result = pca.fit_transform(X)
+        pyplot.scatter(result[:, 0], result[:, 1])
+        words = list(model.wv.vocab)
+        for i, word in enumerate(words):
+        	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+        pyplot.show()
+
     return formatted
 
-# print time.time() - t
+
+def token(text):
+    if not type(text[0]) == list:
+        raise TypeError
+    tokens = [[word_tokenize(i.replace("/", " ").replace("\"", "")) for i in packet] for packet in text]
+    return [[[word.lower() for word in i if not word.lower() in string.punctuation if not "'" in word] for i in packet] for packet in tokens]
+
+def flatten(l, iter=float("inf")):
+    result = []
+    for el in l:
+        if iter == 0:
+            result.append(l)
+            break
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el, iter - 1))
+        else: result.append(el)
+    return result
+
+def ask(ask):
+    while True:
+        result = raw_input(ask)
+        if result == "" or result == "ALL": yield result; break
+        else: yield result; continue
+
+# print "time:", round(timeself.time()- t, 3)
--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import time
 t = time.time()
-import os, re, json, subprocess, warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-import format
+import os, re, json, subprocess, numpy as np, nltk
+import math, string
+from format import *
 from nn.grading import framework
+from nltk.corpus import stopwords
+sw = stopwords.words("english")
 
-p = format.get_packet()
-packet = p[0]
-paths = p[1]
-
-for i, val in enumerate(paths):
-    fuzzy = format.find_packet("fuzzy.txt", re.sub(r"data-\d{3}\.json", "", paths[i]).replace(" ", "\\ "), dir=False)
-    jeff = open(paths[i]).read()
+p = packet("CO-18-M205-S")#, list(ask("Directory name of a packet: ")))
+#write the filepath to data
+for i, val in enumerate(p.paths):
+    fuzzy = find_packet("fuzzy.txt", re.sub(r"data-\d{3}\.json", "", p.paths[i]).replace(" ", "\\ "), dir=False)
+    jeff = open(p.paths[i]).read()
     with open(val, "r+") as f:
         f.write(re.sub(r"future-scene\": \".*?\"", "future-scene\": \"{}\"".format(fuzzy),
                 re.sub(r"data-filepath\": \".*?\"", "data-filepath\": \"{}\"".format(val), jeff)))
 
-dadata = [json.loads(open(i).read()) for i in paths]
-d = [list(format.text(r".*?text", data)) for data in dadata]
-vecs = format.word2vec(d, packet)
+#loads found json files and finds all text to be vectorized
+dadata = [json.loads(open(i).read()) for i in p.paths]
+dtext = [list(getData(r".*?text", data)) for data in dadata]
+#add the fuzzy text to become word vectors
+for i, ptext in enumerate(dtext):
+    ptext.append(open(dadata[i]["meta"]["future-scene"]).read())
+#tokens text
+tokens = token(dtext)
+OGtokens = sorted(list(set([word for packet in tokens for token in packet for word in token])))
+#Word2Vec alg applied and creates vecs
+vecs = vectorize(flatten(tokens, 2), show=False, size=100)
+#tokens and joins data for Categorizing NN
+cdata = [data["challenges"] for data in dadata]
+challenges = categorizedData(cdata)
+sdata = [data["solutions"] for data in dadata]
+solutions = categorizedData(sdata)
 
-
-#    Neural
-#    Network
-#     Stuff
 
 print "time:", round(time.time()- t, 3)
