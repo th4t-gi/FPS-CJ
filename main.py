deeb2898bacf1545968aeaf0de53c0aa4e0e2bfb
th4t-gi <juddbrau@gmail.com>
2018-10-26 20:41:47 -0600

    Minor Editttttts

--- main.py
+++ main.py
@@ -3,26 +3,34 @@ t = time.time()
 import os, re, json, subprocess, numpy as np, nltk
 import math, string
 from format import *
-from keras_attention.lstm import *
 
-p = Getpacket("CO-17-J102-Q")#, list(ask("Directory name of a packet: ")))
+if get_version() > 10.11:
+    from keras_attention.lstm import *
+
+p = Getpacket("ALL")
 
 #loads found json files and finds all text to be vectorized
 dadata = [json.loads(open(i).read()) for i in p.paths]
-dtext = [list(getData(r".*?text", data)) for data in dadata]
+dtext = [list(get_values(r".*?text", data)) for data in dadata]
 #add the fuzzy text to become word vectors
 for i, ptext in enumerate(dtext):
     ptext.append(open(dadata[i]["meta"]["future-scene"]).read())
 #Word2Vec alg applied and creates vecs
 tokens = tokenize(dtext)
 vecs = vectorize(flatten(tokens, 2), show=False, size=100)
-OGtokens = sorted(list(set([word for packet in tokens for token in packet for word in token])))
-
+OGtokens = sorted(set(flatten(tokens)))
 
 #challenges data
 cdata = [data["challenges"] for data in dadata]
 crdata = [data["criteria"] for data in dadata]
 challenges = [PairedData(c, vecs) for packet in cdata for c in packet["data"]]
+
+# for c in challenges:
+#     print c.words
+#     print c.vecs
+#     print c.category
+#     print c.c
+#     print "\n"
 
 # criteria data
 
@@ -31,4 +39,4 @@ challenges = [PairedData(c, vecs) for packet in cdata for c in packet["data"]]
 # Network
 # Stuff
 
-gettime(t).final()
+get_time(t).final()
