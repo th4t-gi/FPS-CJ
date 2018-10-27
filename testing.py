ce797566c2fec848e32d85115767fd9011749358
th4t-gi <juddbrau@gmail.com>
2018-10-26 20:32:07 -0600

    I think this is the model I'll need for the first part of Computer Judge

--- testing.py
+++ testing.py
@@ -1,20 +1,25 @@
-from format import gettime
-t = gettime()
-import keras
-from keras.layers import LSTM, Bidirectional, Dense, Input, Embedding
+import time
+t = time.time()
+# import keras.backend as K
+from keras.layers import LSTM, Bidirectional, Dense, Input
 from keras.utils import plot_model
 from keras.models import Model
 import matplotlib
 from keras_attention.lstm import *
 
-main_input = Input(shape=(100,))
-x = Embedding(output_dim=512, input_dim=10000)(main_input)#, input_length=100)(main_input)
-lstm_out = LSTM(32)(x)
-print lstm_out
+input = Input(shape=(None, 100))
+lstm_out = Bidirectonal(LSTM(32, return_sequences=True)(input))
+output = Dense(22, activation='softmax')(lstm_out)
 
+model = Model(inputs=inputs, outputs=output)
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+print model.summary()
+some_data = None
+some_labes = None
+model.fit(some_data, some_labels, epochs=100)
 # model_attention_applied_after_lstm()
 
-t.final()
+get_time(t).final()
 
 # PURPLE = '\033[95m'
 # CYAN = '\033[96m'
