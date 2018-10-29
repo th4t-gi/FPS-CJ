2f77494acfa9d53a4f58b42605a725f420459e40
Judson Brau <Judd@Hephaestus.local>
2018-10-28 18:33:02 -0600

    I just need to stage all of these real quick

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ t = time.time()
 import os, re, json, subprocess, numpy as np, nltk
 import math, string
 from format import *
+from models import *
 
 if get_version() > 10.11:
     from keras_attention.lstm import *
 
-p = Getpacket("ALL")
+p = Getpacket("CO-18-M205-S")
 
 #loads found json files and finds all text to be vectorized
 dadata = [json.loads(open(i).read()) for i in p.paths]
@@ -21,10 +22,14 @@ vecs = vectorize(flatten(tokens, 2), show=False, size=100)
 OGtokens = sorted(set(flatten(tokens)))
 
 #challenges data
-cdata = [data["challenges"] for data in dadata]
+cdata = [[data["challenges"], data["solutions"]] for data in dadata]
 crdata = [data["criteria"] for data in dadata]
-challenges = [PairedData(c, vecs) for packet in cdata for c in packet["data"]]
-
+# challenges = [PairedData(c, vecs) for packet in cdata for c in packet["data"]]
+# cats = PairedData(cdata, vecs)``
+ddata = flatten([i["data"] for i in flatten(cdata)])
+cats = PairData(ddata, vecs, sorted=True)
+cmodel = Categorizing_model()
+cmodel.fit(cats[0][0], cats[1][0])#, batch_size=len(cats[0]))
 # for c in challenges:
 #     print c.words
 #     print c.vecs
