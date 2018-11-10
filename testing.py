897fd48f021618818a90efa31aefb7f3500046b7
th4t-gi <juddbrau@gmail.com>
2018-11-10 16:20:33 -0700

    Testing... 1 2 3... Testing

--- testing.py
+++ testing.py
@@ -1,20 +1,20 @@
 import time
 t = time.time()
-# import keras.backend as K
+import subprocess
+
 # from keras.layers import LSTM, Bidirectional, Dense, Input
 # from keras.utils import plot_model
 # from keras.models import Model
 # import matplotlib
-from format import get_time, flatten
-
-input = Input(shape=(None, 100))
-lstm_out = Bidirectional(LSTM(32, return_sequences=True))(input)
-output = Dense(22, activation='softmax')(lstm_out)
-
-model = Model(inputs=input, outputs=output)
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-plot_model(model, to_file='categorizing_model.png')
-
+# from format import get_time, flatten
+#
+# input = Input(shape=(None, 100))
+# lstm_out = Bidirectional(LSTM(32, return_sequences=True))(input)
+# output = Dense(22, activation='softmax')(lstm_out)
+#
+# model = Model(inputs=input, outputs=output)
+# model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+# plot_model(model, to_file='categorizing_model.png')
 
 get_time(t).final()
 
